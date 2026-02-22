95bbc1558e4d89d6f85a9c4090ed5ffcb5e9f789
DaishoYokoyama <daisyou117@gmail.com>
2026-02-22 15:21:34 +0900

    feat(works): add imagePosition field to works schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,18 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
+const imagePositionValues = [
+  "top",
+  "center",
+  "bottom",
+  "left",
+  "right",
+  "left-top",
+  "left-bottom",
+  "right-top",
+  "right-bottom",
+] as const;
+
 const works = defineCollection({
   loader: glob({ pattern: "**/index.md", base: "./src/content/works" }),
   schema: ({ image }) =>
@@ -9,6 +21,7 @@ const works = defineCollection({
       description: z.string(),
       image: image(),
       order: z.number(),
+      imagePosition: z.enum(imagePositionValues).default("bottom"),
     }),
 });
 
